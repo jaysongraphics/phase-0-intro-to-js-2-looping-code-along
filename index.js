c0d32d677b5140afd6e4702c7fdc43c76b5b7b87
Edward <jaysongraphics@gmail.com>
2021-04-23 19:29:11 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    const messages = []
+    for (let i = 0; i < names.length; i++) {
+       messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+
+function countDown(numbers) {
+  while (0 < numbers) {
+    console.log(numbers)
+    numbers -=1
+  }
+  console.log(numbers);
+} 
